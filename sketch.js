5ebcd6afedd19d8f0d81744df882809e4fdca6fc
kabeerWadhwa <68274419+kabeerWadhwa@users.noreply.github.com>
2020-08-14 15:58:57 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1, pig1;
+//function preload
+function preload(){
+    backgroundImage = loadImage("sprites/bg.png")
+}
+//function setup
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    //ground sprite
+    ground = new Ground(600,height,1200,20)
+
+    //box 1 and 2
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    //pig 1
+    pig1 = new Pig(810, 350);
+    //log 1
+    log1 = new Log(810,260,300, PI/2);
+
+    //box 3 and 4
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    //pig 3
+    pig3 = new Pig(810, 220);
+    //log 3
+    log3 =  new Log(810,180,300, PI/2);
+    //box 5
+    box5 = new Box(810,160,70,70);
+    //log 4 and 5
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+    //bird
+    bird = new Bird(100,100);
+
+}
+
+function draw(){
+    background(backgroundImage);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+}
